e8015123c1e55de35e9ae7ee5701c51af3a5ca71
Ben Weinstein <benweinstein@Bens-MacBook-Pro.local>
2022-02-12 14:15:35 -0800

    add client

--- prepare_raw_data.py
+++ prepare_raw_data.py
@@ -1,4 +1,7 @@
 #Create train test split
 from src.data import TreeData
-data_module = TreeData("data/raw/neon_vst_data_2022.csv")
+from src import start_cluster
+
+client = start_cluster.start(cpus=100)
+data_module = TreeData("data/raw/neon_vst_data_2022.csv", client = client)
 data_module.setup()
